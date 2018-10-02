1d0a30d7a3c6398124ddb2db17f0f5e06d40c1a0
Victor Luiz <victor.smp13@gmail.com>
2018-10-01 21:00:57 -0300

    Adicionado enunciado do exercício

---
+++ Lista de Exercícios 1/exercicio6.py
@@ -0,0 +1,31 @@
+"""
+    Victor Luiz da Silva Mariano Pereira
+
+    Lista de Exercícios 1
+    Exercício 6
+
+    Descrição:
+        Dois de seus amigos possuem, cada um, R$ 10.000 para investir. O
+        primeiro deseja escolher entre dois investimentos A e B e o segundo
+        entre os investimentos C e D. Alguns destes investimentos possuem
+        limites mínimos e/ou m ́aximos de quantia a ser investida. Estas
+        informações e o rendimento anual de cada investimento são apresentados
+        na tabela a seguir.
+
+        Investimento |  Min         Max             Rendimento
+        ------------------------------------------------------
+            A        |  0       não há limite            9,0%
+            B        |  1000        5000                11,0%
+            C        |  500     não há limite           10,5%
+            D        |  0           3000                11,5%
+
+        (a) Modele o problema de decisão do primeiro amigo,
+        z1 = max{c^T x : Ax <= b}.
+        (b) Modele o problema de decisão do segundo amigo,
+        z2 = max{c^T x : Cx <= d}.
+        (c) Desenhe a região factível dos dois problemas. Qual a solução
+        ótima, z1 e z2, de cada um deles?
+        (d) Você também possui R$ 10.000 e deseja escolher em qual das quatro
+        opções investir. Em outras palavras, você deseja resolver o problema.
+        z3 = max{c^T x : Ax <= b, Cx <= d}. Qual a relação entre z1, z2, z3?
+"""
